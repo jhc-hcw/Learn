d498216beebb9becac87bf4087de32f8e7e2509d
jhc-hcw <2654192132@qq.com>
2022-07-28 11:17:18 +0800

    day2

--- Day.md
+++ Day.md
@@ -17,4 +17,9 @@ sudo dpkg -i path_to_deb_file  /*安装deb包*/
   ```
 - 将本地的master分支推送到远程仓库
 
+```test
+git pull 
+```
+- 等同于git fetch先下载另一个仓库的分支在进行给git merge  
+
   
--- j_for_net/file9/client1.cpp
+++ j_for_net/file9/client1.cpp
@@ -6,4 +6,15 @@
 #include<unistd.h>
 #include<netinet/in.h>
 #include<signal.h>
-
+using namespace std;
+extern void sig_process(int signo);
+extern void sig_pipe(int signo);
+static const s;
+void sig_process_client(int signo){
+    cout<<"Catch a exit signal"<<endl;
+    close(s);
+    exit(0);
+}
+int main(int argc,char * argv[]){
+    
+}
