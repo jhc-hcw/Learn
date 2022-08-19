013d9fb2721b96f7d5f4b5c2b149a2f947470f04
jhc-hcw <2654192132@qq.com>
2022-08-19 20:15:59 +0800

    jhc

--- test/d819/t1.cpp
+++ test/d819/t1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 void merg_parilation(int *data,const int len){
     const int mid=len/2;
     int cache[len];
@@ -17,21 +18,22 @@ void merg_parilation(int *data,const int len){
         if(data[k]<data[j]){
             cache[i]=data[k];
             k++;
+            i++;
         }else{
             cache[i]=data[j];
             j++;
+            i++;
         }
     }
     for(;j<mid;j++,i++) cache[i]=data[j];
     for(;k<len;k++,i++) cache[i]=data[k];
     for(int q=0;q<len;q++) data[q]=cache[q];
 }
-#include<iostream>
 using namespace std;
 int main(){
-    int a[]={3,2,3,4,5,6,7,8,9,0,1,1,2,23,23,2,23,3,23,23,3,423,45,65,65,756,654,435,324234,345,564,456,456,4235,234,3124,231,324,435,453,342,231};
+    int a[]={5,4,3,34,3,34,32,34,3,231,23,342,54,345,34,435,435,34,34,435,345,456,67,867,78,65,-2,54,45,68,453,45,56,345,56,234,345,534,2345,345,234,5346,4};
     int len = sizeof(a)/4;
-    //merg_parilation(a, len);
+    merg_parilation(a, len);
     for(int i=0;i<len;i++){
         cout<<a[i]<<" ";
     }
